16296db01fa3afba89a714ae255b843526f46b08
Anitha L <Anitha L@DESKTOP-98O5IH4>
2024-07-24 21:11:30 +0530

    done

---
+++ Practice/src/main/java/SamplePractice/MethodDe.java
@@ -0,0 +1,22 @@
+package SamplePractice;
+
+public class MethodDe {
+	public static void add(int a,int b)
+	{
+	int c=a+b;
+	System.out.println(c);
+	}
+public static void add(float a,float b)
+{
+	
+float c=a+b;
+System.out.println(c);
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+MethodDe.add(5, 10);
+MethodDe.add(25.3f, 55.4f);
+	}
+
+}
